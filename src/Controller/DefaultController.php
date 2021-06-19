61e576ec687f53a35b8fb0750c68deb87a6c47d6
andsaru <andsaru@hotmail.es>
2021-06-20 00:05:40 +0200

    Solucionar el problema de referencia circular para los JSON

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -96,29 +96,34 @@ class DefaultController extends AbstractController
         $result = $request->query->has('id') ?
             $employeeRepository->find($request->query->get('id')) :
             $employeeRepository->findAll();
+        
+        if ($result instanceof Employee) {
+            return $this->json($this->normalizeEmployee($result));
+        }
 
         $data=[];
 
         foreach ($result as $employee) {
-            $projects = [];
+            // $projects = []; YODA ESTA PARTE LA PASAMOS ABAJO
 
-            foreach ($employee->getProjects() as $project) {
-                array_push($projects, [
-                    'id' => $project->getId(),
-                    'name' => $project->getName(),
-                ]);
-            }
+            // foreach ($employee->getProjects() as $project) {
+            //     array_push($projects, [
+            //         'id' => $project->getId(),
+            //         'name' => $project->getName(),
+            //     ]);
+            // }
 
-            array_push($data, [
-                'name' => $employee->getName(),
-                'email' => $employee->getEmail(),
-                'city' => $employee->getCity(),
-                'department' => [
-                    'id' => $employee->getDepartment()->getId(),
-                    'name' => $employee->getDepartment()->getName(),
-                ],
-                'projects' => $projects
-            ]);
+            // array_push($data, [
+            //     'name' => $employee->getName(),
+            //     'email' => $employee->getEmail(),
+            //     'city' => $employee->getCity(),
+            //     'department' => [
+            //         'id' => $employee->getDepartment()->getId(),
+            //         'name' => $employee->getDepartment()->getName(),
+            //     ],
+            //     'projects' => $projects
+            // ]);
+            array_push($data, $this->normalizeEmployee($employee));
         }
                return $this->json($data);
     }
@@ -155,4 +160,33 @@ class DefaultController extends AbstractController
     // EJERCICIO
     // Crear la el recurso para obtener una representación
     // de "UN" empleado en formato JSON.
+
+        /**
+     * Normalize an employee.
+     * 
+     * @param Employee $employee
+     * 
+     * @return array|null
+     */
+    private function normalizeEmployee (Employee $employee): ?array {
+        $projects = [];
+
+        foreach($employee->getProjects() as $project) {
+            array_push($projects, [
+                'id' => $project->getId(),    
+                'name' => $project->getName(),    
+            ]);
+        }
+
+        return [
+            'name' => $employee->getName(),
+            'email' => $employee->getEmail(),
+            'city' => $employee->getCity(),
+            'department' => [
+                'id' => $employee->getDepartment()->getId(),
+                'name' => $employee->getDepartment()->getName(),
+            ],
+            'projects' => $projects
+        ];
+    }
 }
